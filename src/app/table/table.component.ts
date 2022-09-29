0c5dc3546a8c0c8badd374a3ae41d5d01e7168ad
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-09-28 23:42:11 -0500

    Make table take sample dollar data

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -9,30 +9,32 @@ export class TableComponent implements OnInit {
 
   constructor() { }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['name', 'buy_price', 'sell_price', 'url'];
+  dataSource: DollarExchange[] = ELEMENT_DATA;
 
   ngOnInit(): void {
   }
 
 }
 
-export interface PeriodicElement {
+export interface DollarExchange {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  buy_price: number;
+  sell_price: number;
+  url: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+const ELEMENT_DATA: DollarExchange[] = [
+  {
+    name: 'test',
+    buy_price: 3.8,
+    sell_price: 3.9,
+    url: 'test.com'
+  },
+  {
+    name: 'test',
+    buy_price: 3.8,
+    sell_price: 3.9,
+    url: 'test.com'
+  },
 ];
