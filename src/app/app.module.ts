f68bf75aadc003e2796eb560300313574220ae38
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-09-28 23:30:54 -0500

    Add angular sample table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
